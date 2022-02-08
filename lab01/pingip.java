d5f4b2fb00c33ee1541ea62918a64be7ec87aaaf
Michael J Wilson <mjwilson@csciarch04.muad.local>
2022-02-07 20:45:19 -0500

    Did python url thing

--- lab01/pingip.java
+++ lab01/pingip.java
@@ -1,20 +1,21 @@
 import java.io.*;
+import java.lang.Integer;
 
 public class Pingip {
 	public static void main(String[] args) {
 
 		/* IP address must be listed before ping count */
 		String address = args[0];
-		int pingCount = args[1];
+		String pingCount = args[1];
 
 		runPingCommand(address, pingCount);
 		java.util.Date date = new java.util.Date();
 		System.out.println(date);
 	}
 
-	public static void runPingCommand(String address, int pingCount) {
+	public static void runPingCommand(String address, String pingCount) {
 		try {
-			Process p = Runtime.getRuntime().exec(___);
+			Process p = Runtime.getRuntime().exec("ping " + address + " -c " + pingCount);
 			BufferedReader InputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
 			String s = "";
--- lab01/rfc.py
+++ lab01/rfc.py
@@ -1,15 +1,15 @@
-import sys, urlib.request
+import sys, urllib.request
 
 try:
-    rf_num = inst(sys.argv[1])
+    rf_num = int(sys.argv[1])
 except (IndexError, ValueError):
     print('Please provide an RFS number as first argument e.g. in Terminal, type: python3 urllib.py RFC num')
     sys.exit(2)
 
 source = 'http://www.rfc-editor.org/rfc/rfc{}.txt'
-url = source.format(____)
-rfc_raw = urlib.request.urlopen(____).read()
+url = source.format(rf_num)
+rfc_raw = urllib.request.urlopen(url).read()
 rfc = rfc_raw.decode()
-print(____)
+print(rfc)
 
 
